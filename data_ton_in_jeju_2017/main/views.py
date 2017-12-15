81f17f43f1524ea875f0e16a0c1c3e5f4ce7270f
sprout <ehdgks0627@naver.com>
2017-12-16 03:05:33 +0900

    Added test function

--- data_ton_in_jeju_2017/main/views.py
+++ data_ton_in_jeju_2017/main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from subprocess import check_output
+
 
 # Create your views here.
+
+def classfiy(requests):
+    return HttpResponse(check_output(["python", "/home/sprout/tmp/tensorflow_object/research/object_detection/go.py",
+                                      "/var/www/html/data-ton-in-jeju/pic1.jpeg"]))
